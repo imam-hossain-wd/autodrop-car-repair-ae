be2113e96b92acd3c067339cc6c55fa2bc16bc1f
imam-hossain-wd <zunayedemon6t9@gmail.com>
2026-08-18 13:02:57 +0600

    added service map section

--- components/pages/HomePage/HomePage.tsx
+++ components/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Hero } from "@/components/view/Hero/Hero";
 import HowItWorks from "@/components/view/HowItWorks/HowItWorks";
+import { ServiceMap } from "@/components/view/ServiceMap/ServiceMap";
 import Extra from "@/components/view/WhoWeAre/Extra";
 import { WhoWeAre } from "@/components/view/WhoWeAre/WhoWeAre";
 import { WhyChooseUs } from "@/components/view/WhyChooseUs/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function HomePage() {
             <HowItWorks />
             <WhoWeAre />
             <WhyChooseUs />
+            <ServiceMap />
         </div>
     )
 }
