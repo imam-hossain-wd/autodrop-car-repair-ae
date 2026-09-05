a2af5aecd3fa1198dcebf6eb23ea4670df007f1d
imam-hossain-wd <zunayedemon6t9@gmail.com>
2026-09-05 23:18:04 +0600

    added policy page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -1,6 +1,10 @@
 import PrivacyPolicyPage from '@/components/pages/PrivacyPage/PrivacyPage'
+import { siteMetadata } from '@/seo/metaData';
+import { Metadata } from 'next';
 
 
+
+export const metadata: Metadata = siteMetadata.privacy;
 export default function page() {
   return <PrivacyPolicyPage />
 }
--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -1,6 +1,9 @@
 import TermsPage from '@/components/pages/TermsPage/TermsPage'
+import { siteMetadata } from '@/seo/metaData'
+import { Metadata } from 'next';
 
 
+export const metadata: Metadata = siteMetadata.terms;
 export default function page() {
   return <TermsPage />
 }
